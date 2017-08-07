0fe426bc676784f9feffc0be5f3c33d3652d499e
Kate Byars and Stephanie Mayer <stephanie@>
2017-08-07 15:46:51 -0700

    Add everything

---
+++ src/main/java/App.java
@@ -0,0 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.Post;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) { //type “psvm + tab” to autocreate this
+        staticFileLocation("/public");
+
+
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //route that makes new post
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<>();
+
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            model.put("post", newPost);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+    }
+}
